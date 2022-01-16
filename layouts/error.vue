b3cd118ad4b7ffa31a5ee804afbc3b83d13d45eb
kkuyu <devils67@naver.com>
2022-01-16 23:53:37 +0900

    Add error

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>{{ error }}</div>
+    <NuxtLink :to="{ path: '/' }">Go Home</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error',
+  name: 'errorLayout',
+  components: {
+    Error,
+  },
+  props: {
+    error: {
+      type: Object,
+      required: false,
+      default: () => {
+        return {};
+      },
+    },
+  },
+  setup(props, context) {},
+};
+</script>
